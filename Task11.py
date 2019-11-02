3cd5121a4d53daa916ef3f7d9048705073eb9bb1
Maks <nazirov.maksat96@gmail.com>
2019-11-02 09:05:48 +0600

    P2_t11

---
+++ Task11.py
@@ -0,0 +1,16 @@
+# In mathematics, the factorial of an integer n, denoted by n! is the following
+# product:
+# n!=1×2×...×n
+# For the given integer n
+# calculate the value n!. Don't use math module in this exercise.
+
+# n=int(input("введите число: "))
+# factorial=1
+# if n<0:
+#   print("error")
+# if n==0:
+#   print("1")
+# else:
+#   for i in range(1,n+1):
+#      factorial=factorial*i
+# print(factorial) 
